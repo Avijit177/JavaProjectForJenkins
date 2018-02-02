b9e8cf002875c31b0f149a35bc0c21053946c8b3
Avijit Kumar <avijit.kumar@bjitgroup.com>
2018-02-02 08:41:35 +0600

    test commit

---
+++ ClassExample/src/Example3.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Objects;
+
+public class Example3 {
+	
+	public static void main(String args[])
+	{
+		System.out.println();
+	}
+
+}
+class Order {
+boolean IsSameString(String s1, String s2){
+	return (Objects.equals(s1,s2));
+}
+}
+
+class Mail {
+	
+static boolean IsSameString(String s1, String s2) {
+	
+	return (Objects.equals(s1,s2));
+}
+}
